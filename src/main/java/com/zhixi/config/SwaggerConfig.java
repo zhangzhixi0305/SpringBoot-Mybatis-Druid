6a1b51f4b6cae79766f7ffa39ea0befde2c24e4a
zhang-zhixi <1820712542@qq.com>
2021-12-14 11:32:23 +0800

    删除一些注解

--- src/main/java/com/zhixi/config/SwaggerConfig.java
+++ src/main/java/com/zhixi/config/SwaggerConfig.java
@@ -5,12 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 
